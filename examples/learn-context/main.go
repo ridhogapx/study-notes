4842e4b6c0fb10b2289688f6525ee4e4035e867a
RageNeko26 <rneko2006@gmail.com>
2023-11-01 22:31:15 +0700

    feat: Add sample of usage with passing value

--- examples/learn-context/main.go
+++ examples/learn-context/main.go
@@ -9,7 +9,16 @@ func Foo(ctx context.Context) {
   fmt.Println("Barrrr")	
 }
 
+func withVal(ctx context.Context) {
+  fmt.Println("The value:", ctx.Value("foo") )
+}
+
 func main() {
   ctx := context.TODO()
   Foo(ctx)
+
+  // With value
+  ctxWithVal := context.WithValue(context.Background(), "foo", "bar")
+ 
+  withVal(ctxWithVal)
 }
